9113fd92299cd100dfe559cc23417dc0d22cfaf8
wzq <295386498@qq.com>
2020-06-30 10:46:15 +0800

    the updated sql statement

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,46 @@
 -- **Database level:**
 -- Display all the database
+show databases;
 
 -- Enter a certain database
+use test;
 
 -- Create a database
+create database test;
 
 -- Create the database of the designated character set
+create database test default character set utf8 collate utf8_general_ci;
 
 -- Display the creation information fo the database
+show create database `test`;
 
--- Revise the codes of the database
+-- Revise the codes of the database (这个不懂。。。)
+set character_set_client=utf8;
 
 -- Delete a database
+drop database test;
 
 -- **Table level**
 -- Revise table name
+alter table test rename student;
 
 -- Revise the field's data type
+alter table student modify column age double;
 
 -- Revise field name
+alter table student change id sid varchar(20);
 
 -- Add field
+alter table student add temp int;
 
 -- Delete field
+alter table student drop temp;
 
 -- Revise the table's storage engine
+alter table student ENGINE = InnoDB;
 
 -- Delete the table's foreign key restriant
+alter table student drop foreign key id;
 
 -- Delete a table
+drop table student;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,11 @@
 -- Insert record
+insert into student(id,name,age,sex) values ('003','Maria',19,'female');
 
 -- Revise record
+update student set id='004' where name='Maria';
 
 --  Delete record
+delete from student where id='002';
 
 -- Search record
+select * from student;
